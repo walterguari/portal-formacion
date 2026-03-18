56a37cea83ece947dc48b594191725b76ce9a502
walterguari <analista.autociel@gmail.com>
2026-03-18 14:43:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -242,14 +242,15 @@ with tab3:
                     }
                 })
 
-        options = {
+      options = {
             "headerToolbar": {"left": "prev,next today", "center": "title", "right": "dayGridMonth,listMonth"},
             "initialView": "dayGridMonth",
             "locale": "es",
             "selectable": True,
+            "height": 600,
         }
         
-        state = calendar(events=calendar_events, options=options, key="calendario_produccion_v1")
+        state = calendar(events=calendar_events, options=options, key="calendario_fuerza_v5")
         
         # MOSTRAR DETALLES AL HACER CLIC
         if state.get("eventClick"):
